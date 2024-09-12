16ba1577772a45fc64b15372f3b2e63ce2ff395f
Joshua <jcv234@nau.edu>
2024-09-11 18:59:03 -0700

    Adding some comments to document what I did
    Signed-off-by: Joshua <jcv234@nau.edu>

--- index.js
+++ index.js
@@ -4,17 +4,24 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+// variable to store blogs
 let blogs = [];
 
+// get it so the static objects can use css
 app.use(express.static("public"));
+
+// parse incoming data 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// renders the main home page 
 app.get("/", (req, res) => {
     res.render("index.ejs", {
         blogs: blogs
     })
 });
 
+// renders the home page after updating blogs with the new blog data
+// uses .toLocaleString() to help make data format look better 
 app.post("/submit", (req, res) => {
     const blog = {
         author: req.body.author,
@@ -26,12 +33,14 @@ app.post("/submit", (req, res) => {
     res.redirect("/");
 });
 
+// Deletes the blog at index returned and fills gap wih function (splice)
 app.post("/delete", (req, res) => {
     const blogIndex = req.body.index;
     blogs.splice(blogIndex, 1);
     res.redirect("/")
 });
 
+// grab the data at index in blogs to load into the for on edit.ejs and render
 app.get("/edit/:index", (req, res) => {
     const blogIndex = req.params.index;
     const blog = blogs[blogIndex];
@@ -41,17 +50,20 @@ app.get("/edit/:index", (req, res) => {
     })
 });
 
+// update the data at index with form data from edit.ejs return to home page
+// uses .toLocaleString() to help make data format look better 
 app.post("/update/:index", (req,res) => {
     const blogIndex = req.params.index;
     blogs[blogIndex] = {
         author: req.body.author,
         blog_title: req.body.blog_title,
         blog: req.body.blog,
-        date: new Date().toLocaleString()
+        date: new Date().toLocaleString() 
     };
     res.redirect("/");
 });
 
+// make web app run on ${port}
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`)
 });
